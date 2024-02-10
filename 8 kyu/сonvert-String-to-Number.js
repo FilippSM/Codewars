c873ae0f00657ac1c8faf8513973f275fc0d49fa
Filipp Sam <philippsm666@gmail.com>
2024-02-10 23:03:31 +0300

    add сonvert-String-to-Number.js

---
+++ 8 kyu/сonvert-String-to-Number.js
@@ -0,0 +1,14 @@
+/* DESCRIPTION
+Given a set of numbers, return the additive inverse of each. 
+Each positive becomes negatives, and the negatives become positives. */
+
+//SOLUTION
+const stringToNumber = function(str){
+    let num = Number(str);
+return num;
+}
+
+console.log("stringToNumber: ", stringToNumber("1234"));  
+console.log("stringToNumber: ", stringToNumber("605"));  
+console.log("stringToNumber: ", stringToNumber("1405"));  
+console.log("stringToNumber: ", stringToNumber("-7"));  
