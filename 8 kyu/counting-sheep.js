05fc0408fb45e535cea3dd4b62f0317fd5426b71
Filipp Sam <philippsm666@gmail.com>
2024-02-14 21:37:34 +0300

    add counting-sheep.js

---
+++ 8 kyu/counting-sheep.js
@@ -0,0 +1,21 @@
+//DESCRIPTION
+//Consider an array/list of sheep where some sheep may be missing from their place. 
+//We need a function that counts the number of sheep present in the array (true means present).
+
+//SOLUTION
+function countSheeps(sheep) {
+    let count = 0;
+    for (i = 0; i < sheep.length; i++) {
+      if (!sheep[i] === false) {
+        count++;
+      } 
+    } 
+    return count; 
+  }
+  
+  console.log("countSheeps: ", countSheeps(undefined));
+  console.log("countSheeps: ", countSheeps(null));
+  console.log("countSheeps: ", countSheeps(false));
+  console.log("countSheeps: ", countSheepss(true));
+  console.log("countSheeps: ", countSheepss([undefined,null,false,true]));
+  console.log("countSheeps: ", countSheepss([true,true,true,false,true,true,true,true,true,false,true,false,true,false,false,true,true,true,true,true,false,false,true,true]));
