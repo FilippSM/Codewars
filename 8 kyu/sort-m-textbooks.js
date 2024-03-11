476239e7a051aaf19507ce5eda5c9f8876060bbb
Filipp Sam <philippsm666@gmail.com>
2024-03-11 22:20:03 +0300

    add sort-m-textbooks.js

---
+++ 8 kyu/sort-m-textbooks.js
@@ -0,0 +1,23 @@
+/* DESCRIPTION
+HELP! Jason can't find his textbook! It is two days before the test date, 
+and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, 
+so he can study before the test.
+
+The sorting should NOT be case sensitive */
+
+// SOLUTION
+function sorter(textbooks) {
+    let a = textbooks.sort((a, b) => {
+        if (a.toLowerCase() < b.toLowerCase()) {
+            return -1;
+        }
+        if (a.toLowerCase() > b.toLowerCase()) {
+            return 1;
+        }
+        return 0;
+    });
+
+    return a;
+};
+
+console.log("sorter: ", sorter(['Algebra', 'History', 'Geometry', 'English']));
