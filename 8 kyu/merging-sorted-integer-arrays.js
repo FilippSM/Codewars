9b18044f51fa73d2c2a9a1866d142724888e765c
Filipp Sam <philippsm666@gmail.com>
2024-03-13 22:34:44 +0300

    add merging-sorted-integer-arrays.js

---
+++ 8 kyu/merging-sorted-integer-arrays.js
@@ -0,0 +1,15 @@
+//DESCRIPTION
+/* Write a function that merges two sorted arrays into a single one. 
+The arrays only contain integers. 
+Also, the final outcome must be sorted and not have any duplicate. */
+
+//SOLUTION
+function mergeArrays(a, b) {
+    const array = a.concat(b);
+    const newArray = [...new Set(array)];
+    
+    return newArray.sort((aa, bb) => aa - bb);
+};
+
+console.log(mergeArrays([1, 3, 5], [2, 4, 6]));
+console.log(mergeArrays([2, 4, 8], [2, 4, 6]));
